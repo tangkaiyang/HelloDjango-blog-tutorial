ad5ee7504ffb80ed60c4272b9b63a50f3a588b8c
tangky <tangky@tangky.com>
2020-03-28 14:02:39 +0800

    debug=False

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'km!20ljxkq^u5dv(nfvsh34gsgxcds=6r&^^mtco_8es3kkb9o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 
